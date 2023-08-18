48bb86c17bd103ff05ce12a9152afeb6511a9ddc
vishal <vishal377@gmail.com>
2023-08-18 14:04:55 +0530

    enable cors

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
