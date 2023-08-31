f6eb5032e557ddfca3b6d19505f623df6876b7da
Shanmugapriya <shanmugapriyadoodleblue@gmail.com>
2023-08-31 14:17:27 +0530

    try catch fixes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,17 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
+  try {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
 
-  // Set JWT as an HTTP-Only cookie
-  res.cookie('jwt', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-  });
-};
+    // Set JWT as an HTTP-Only cookie
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+      sameSite: 'strict', // Prevent CSRF attacks
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+  } catch (error) {
+    console.log(error, "error")
+    return error
+  }
+}
 
 export default generateToken;
